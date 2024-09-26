a13e3c3398349a76de698d4c43ba4c0fba5dcf11
unknown <you@example.com>
2024-09-26 10:07:27 +0300

    izdariju paris lietas

---
+++ cli/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Piesakies webināram!”</h1>
+    
+
+    
+
+
+    <select name="select">
+  <option value="first">Beginner</option>
+  <option value="second">Intermediate</option>
+  <option value="third">Advanced</option>
+    </select>
+    <fieldset>
+        
+    <legend>Vai vēlies saņemt info par citiem mūsu organizētajiem pasākumiem?</legend>
+    <label><input type="radio" name="radio" value="yes" /> Yes</label>
+    <label><input type="radio" name="radio" value="no" /> No</label>
+  </fieldset>
+</body>
+</html>
